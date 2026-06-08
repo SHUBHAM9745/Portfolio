891931e7fa7e056680e662cf023276b523cd0120
Shubham Mishra <shubhammishra430052@gmail.com>
2026-06-09 05:10:51 +0530

    Fix CV filename

--- Portfolio-main/src/components/Resume/ResumeNew.js
+++ Portfolio-main/src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Shubham_FullStack Dev.pdf";
+import pdf from "../../Assets/Shubham_FullStack.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
